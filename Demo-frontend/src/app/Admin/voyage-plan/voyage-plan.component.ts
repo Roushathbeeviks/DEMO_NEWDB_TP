2b931df1610f6289aec73e2e44116d976908afac
Abhinand0514 <abhinandsunilk@gmail.com>
2023-01-17 18:39:20 +0530

    change

--- Demo-frontend/src/app/Admin/voyage-plan/voyage-plan.component.ts
+++ Demo-frontend/src/app/Admin/voyage-plan/voyage-plan.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VesselService } from 'src/app/services/vessel.service';
+import { VoyageplanService } from 'src/app/services/voyageplan.service';
 @Component({
   selector: 'app-voyage-plan',
   templateUrl: './voyage-plan.component.html',
@@ -8,11 +10,63 @@ import { Router } from '@angular/router';
 })
 export class VoyagePlanComponent implements OnInit {
 
-  constructor(private route:Router) { }
 
+  public date = new Date()
+  voyageForm:any= FormGroup;
+  StartPort:any
+  DestinationPort:any
+  vesselId:any;
+  constructor(
+    private voyageserv:VoyageplanService,
+    private route:ActivatedRoute,
+    private router:Router,
+    private formBuilder: FormBuilder,
+    private vesselserv:VesselService
+    ) { }
   ngOnInit(): void {
+
+    this.voyageForm=this.formBuilder.group
+    ({
+      startport_id:[],
+      destinationport_id:[],
+      cosp_lat:[],
+      cosp_long:[],
+      eosp_lat:[],
+      eosp_long:[],
+      cosp_time:[],
+      displacement:[],
+      earliest_eta:[],
+      just_eta:[],
+      vessel_id:[]
+    
+    });
+
+    this.voyageserv.GetStartPort().subscribe(startport =>{
+      this.StartPort = startport
+    })
+    this.voyageserv.GetDestinationPort().subscribe(endport =>{
+      this.DestinationPort=endport
+  })
+  this.vesselId=this.route.snapshot.paramMap.get('id')
   }
-  
-  
+  save()
+ {
+  this.voyageForm.value.vessel_id=this.vesselId
+  this.voyageserv.VoyageForm(this.voyageForm.value).subscribe((res:any)=>
+  {
+    if(res?.success)
+    {
+      this.vesselserv.openSnackBar("Successfully save","ok")
+      this.router.navigate(['/voyagetable/',this.vesselId])
+      
+    }
+  })
+
+  // this.vesselserv.openSnackBar("Successfully save","ok")
+}
+
+reloadPage(): void {
+  window.location.reload();
+}
 
 }
--- Demo-frontend/src/app/voyage/voyage-form/voyage-form.component.ts
+++ Demo-frontend/src/app/voyage/voyage-form/voyage-form.component.ts
@@ -28,8 +28,8 @@ export class VoyageFormComponent implements OnInit {
 
     this.voyageForm=this.formBuilder.group
     ({
-      startport_id:[],
-      destinationport_id:[],
+      startport_id:[''],
+      destinationport_id:[''],
       cosp_lat:[],
       cosp_long:[],
       eosp_lat:[],
@@ -69,5 +69,8 @@ export class VoyageFormComponent implements OnInit {
 reloadPage(): void {
   window.location.reload();
 }
+reset(){
 
+  
+}
 }
