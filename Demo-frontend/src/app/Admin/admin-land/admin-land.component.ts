339f622eb26c7fe5317d248868501b4f7657269d
Abhinand0514 <abhinandsunilk@gmail.com>
2023-01-24 17:31:48 +0530

    SELECT FOR SEARCH and Uniformity

--- Demo-frontend/src/app/Admin/admin-land/admin-land.component.ts
+++ Demo-frontend/src/app/Admin/admin-land/admin-land.component.ts
@@ -10,6 +10,30 @@ import { VoyageplanService } from 'src/app/services/voyageplan.service';
 })
 
 export class AdminLandComponent implements OnInit {
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['&#8249', '&#8250'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 data: any;
 id:any[]=[];
 vesselId:any
@@ -111,71 +135,5 @@ vesselId:any
   {
     this.route.navigate(['/vessel']);
   }
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
-    navSpeed: 700,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 5
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
-      }
-    },
-    nav: true
-  }
-data: any;
-id:any[]=[];
-vesselId:any
-stportId:any[]=[]
-stPort:any[]=[]
-dstPortId:any[]=[]
-dstPort:any[]=[]
- value:any
-  constructor(private route:Router,
-    private vesselserv:VesselService,private voyageserv:VoyageplanService) { }
-
-  ngOnInit(): void {
-  
-
-  }
-  navigate()
-  {
-    this.route.navigate(['/vessel']);
-  }
-  // customOptions: OwlOptions = {
-  //   loop: false,
-  //   mouseDrag: false,
-  //   touchDrag: false,
-  //   pullDrag: false,
-  //   dots: false,
-  //   navSpeed: 600,
-  //   navText: ['&#8249', '&#8250;'],
-  //   responsive: {
-  //     0: {
-  //       items: 4
-  //     },
-  //     400: {
-  //       items: 2
-  //     },
-  //     760: {
-  //       items: 3
-  //     },
-  //     1000: {
-  //       items: 4
-  //     }
-  //   },
-  //   nav: true
-  // }
+ 
 }
